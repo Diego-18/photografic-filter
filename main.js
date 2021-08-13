bf8fb3e6c284ae2d3c2edaac8f3d3987017ddf34
Diego Chavez <ingdiegochavez18@gmail.com>
2021-08-13 01:36:35 -0400

    core/new-version (#1)
    * core/new-version
    
    ** Use of Flexbox.
    ** Improved responsive design.
    ** Code optimization and JS integration.
    ** Bootstrap removed.
    
    * Update README.md

---
+++ main.js
@@ -0,0 +1,9 @@
+function fjOption(vpOption){
+    // alert('Prueb');
+    let Option = vpOption;
+    let Item = document.getElementById('img__demo');
+
+    if(Option != ''){
+        Item.className = Option;
+    }
+}
